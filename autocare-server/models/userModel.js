15f7775b1a1ef27af79ef5ccb00503dafe0d8e2d
MhmdAtout <mohamad.atout0@gmail.com>
2023-04-14 21:27:25 +0300

    backkend: creating user model

---
+++ autocare-server/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	image: {
+		type: Text,
+	},
+	type: {
+		type: String,
+		enum: ["customer", "admin"],
+		default: "customer",
+	},
+	favourites: {
+		type: Array,
+	},
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
