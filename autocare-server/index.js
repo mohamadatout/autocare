505af35f77893343283637e71dd6d06f491263d2
MhmdAtout <mohamad.atout0@gmail.com>
2023-04-14 22:48:56 +0300

    backend: grouping authentication routes

--- autocare-server/index.js
+++ autocare-server/index.js
@@ -3,6 +3,9 @@ const app = express();
 require("dotenv").config();
 app.use(express.json());
 
+const authRouter = require("./routes/auth.routes");
+app.use("/auth", authRouter);
+
 app.listen(process.env.PORT, (err) => {
 	if (err) console.error(err);
 	console.log("Server is running on port ", process.env.PORT);
