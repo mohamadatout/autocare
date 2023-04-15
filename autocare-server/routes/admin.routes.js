34539872bd3ead75ca79b06f3f763d808022dddf
MhmdAtout <mohamad.atout0@gmail.com>
2023-04-15 11:13:42 +0300

    backend: adding a route for changeSubscription API

--- autocare-server/routes/admin.routes.js
+++ autocare-server/routes/admin.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const router = Router();
 
-const { newStore } = require("../controllers/admin.controller");
+const {
+	newStore,
+	changeSubscription,
+} = require("../controllers/admin.controller");
 
 router.post("/newStore", newStore);
+router.post("/changeSubscription", changeSubscription);
 
 module.exports = router;
