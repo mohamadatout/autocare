18b594ef495cc4e5bd7c5d0456264646818deb0e
MhmdAtout <mohamad.atout0@gmail.com>
2023-04-15 13:42:18 +0300

    backend: changing reference to the User model

--- autocare-server/controllers/admin.controller.js
+++ autocare-server/controllers/admin.controller.js
@@ -1,21 +1,22 @@
-const Store = require("../models/storeModel");
+const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 
 exports.newStore = async (req, res) => {
 	const { name, email, password } = req.body;
 
-	const existingStore = await Store.findOne({ email });
+	const existingStore = await User.findOne({ email });
 	if (existingStore) {
 		return res.status(409).json({
 			message: "Email already associated with another account",
 		});
 	}
 
-	const store = new Store();
+	const store = new User();
 	const hashedPassword = await bcrypt.hash(password, 10);
 	store.name = name;
 	store.email = email;
 	store.password = hashedPassword;
+	store.type = "user";
 	store.cover_img = "";
 	store.logo_img = "";
 	store.items = [];
@@ -30,10 +31,10 @@ exports.newStore = async (req, res) => {
 exports.changeSubscription = async (req, res) => {
 	const { _id } = req.body;
 
-	const store = await Store.findOne({ _id });
+	const store = await User.findOne({ _id });
 	const opposite = !store.subscription;
 
-	const updatesStore = await Store.findOneAndUpdate(
+	const updatesStore = await User.findOneAndUpdate(
 		{ _id },
 		{ $set: { subscription: opposite } },
 		{ new: true }
