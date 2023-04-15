34392a8db3463181cf2e8893023eac790c21bd00
MhmdAtout <mohamad.atout0@gmail.com>
2023-04-15 14:11:27 +0300

    backend: adding a route for get messages API

--- autocare-server/routes/actions.routes.js
+++ autocare-server/routes/actions.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const router = Router();
 
-const { sendMessage } = require("../controllers/actions.controller");
+const {
+	sendMessage,
+	getMessages,
+} = require("../controllers/actions.controller");
 
 router.post("/sendMessage", sendMessage);
+router.post("/getMessages", getMessages);
 
 module.exports = router;
