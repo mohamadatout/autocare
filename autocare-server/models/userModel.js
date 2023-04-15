ada26e3de9be84eb7b9fc9cfdf0c801ab5e99ef9
MhmdAtout <mohamad.atout0@gmail.com>
2023-04-15 13:39:18 +0300

    backend: editing users model

--- autocare-server/models/userModel.js
+++ autocare-server/models/userModel.js
@@ -1,5 +1,45 @@
 const mongoose = require("mongoose");
 
+const itemSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	made: {
+		type: String,
+	},
+	model: {
+		type: String,
+	},
+	year: {
+		type: String,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+	image: {
+		type: String,
+	},
+	category: {
+		type: String,
+		enum: ["Interior", "Exterior", "Motor", "Parts", "Washing"],
+		required: true,
+	},
+});
+
+const reviewSchema = new mongoose.Schema({
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	review: {
+		type: String,
+		required: true,
+	},
+});
+
 const userSchema = new mongoose.Schema({
 	name: {
 		type: String,
@@ -14,16 +54,30 @@ const userSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	type: {
+		type: String,
+		enum: ["customer", "user", "admin"],
+		default: "customer",
+	},
+	subscription: {
+		type: Boolean,
+		default: true,
+		required: true,
+	},
 	image: {
 		type: String,
 	},
-	type: {
-		type: String,
-		enum: ["customer", "admin"],
-		default: "customer",
-	},
 	favourites: {
 		type: Array,
+		default: null,
+	},
+	items: {
+		type: [itemSchema],
+		default: null,
+	},
+	reviews: {
+		type: [reviewSchema],
+		default: null,
 	},
 });
 
