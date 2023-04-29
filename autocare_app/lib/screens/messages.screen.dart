18cb56a29b434110235b7ea10ddd6c6fcb402e35
MhmdAtout <mohamad.atout0@gmail.com>
2023-04-29 11:10:53 +0300

    frontend: creating and designig messages screen

---
+++ autocare_app/lib/screens/messages.screen.dart
@@ -0,0 +1,35 @@
+import 'package:autocare_app/widgets/Messages.dart';
+import 'package:autocare_app/widgets/SendMessageBar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MessagesScreen extends StatefulWidget {
+  const MessagesScreen({super.key});
+
+  @override
+  State<MessagesScreen> createState() => _MessagesScreenState();
+}
+
+class _MessagesScreenState extends State<MessagesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          "Mhmd Al Agha",
+          style: TextStyle(color: Colors.black),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: Stack(
+        children: [
+          Positioned(child: Messages()),
+          SendMesssageBar(),
+        ],
+      ),
+    );
+  }
+}
