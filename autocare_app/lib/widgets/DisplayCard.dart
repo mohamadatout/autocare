b9dc762c0285b96d9c51dd0c1628e7fda7e21f60
MhmdAtout <mohamad.atout0@gmail.com>
2023-04-30 13:12:25 +0300

    frontend: Creating and designing a display card

---
+++ autocare_app/lib/widgets/DisplayCard.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class DisplayCard extends StatelessWidget {
+  final String imgURL;
+  final String text;
+  final double imgWidth;
+  final double imgHeight;
+
+  const DisplayCard({
+    super.key,
+    required this.text,
+    required this.imgURL,
+    required this.imgHeight,
+    required this.imgWidth,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 15),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset(
+              imgURL,
+              width: imgWidth,
+              height: imgHeight,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            text,
+            style: TextStyle(
+              fontSize: 16,
+              fontFamily: "sora",
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
