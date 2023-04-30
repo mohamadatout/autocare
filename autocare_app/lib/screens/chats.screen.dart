1a8e9b8dc879d59279b426ccae3b25db6d2464f6
MhmdAtout <mohamad.atout0@gmail.com>
2023-04-30 18:39:50 +0300

    frontend: removing appbar and bottom navigator from chats screen

--- autocare_app/lib/screens/chats.screen.dart
+++ autocare_app/lib/screens/chats.screen.dart
@@ -30,79 +30,20 @@ class _ChatsScreenState extends State<ChatsScreen> {
       "time": "15 min",
       "imgURL": "assets/google.png",
     },
-    {
-      "name": "Mhmd Al Agha",
-      "lastMessage": "Hii",
-      "time": "15 min",
-      "imgURL": "assets/google.png",
-    },
-    {
-      "name": "Mhmd Al Agha",
-      "lastMessage": "Hii",
-      "time": "15 min",
-      "imgURL": "assets/google.png",
-    },
-    {
-      "name": "Mhmd Al Agha",
-      "lastMessage": "Hii",
-      "time": "15 min",
-      "imgURL": "assets/google.png",
-    },
-    {
-      "name": "Mhmd Al Agha",
-      "lastMessage": "Hii",
-      "time": "15 min",
-      "imgURL": "assets/google.png",
-    },
-    {
-      "name": "Mhmd Al Agha",
-      "lastMessage": "Hii",
-      "time": "15 min",
-      "imgURL": "assets/google.png",
-    },
-    {
-      "name": "Mhmd Al Agha",
-      "lastMessage": "Hii",
-      "time": "15 min",
-      "imgURL": "assets/google.png",
-    },
-    {
-      "name": "Mhmd Al Agha",
-      "lastMessage": "Hii",
-      "time": "15 min",
-      "imgURL": "assets/google.png",
-    },
-    {
-      "name": "Mhmd Al Agha",
-      "lastMessage": "Hii",
-      "time": "15 min",
-      "imgURL": "assets/google.png",
-    },
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text(
-          "Chats",
-          style: TextStyle(color: Colors.black),
-        ),
-        elevation: 0,
-        backgroundColor: Colors.white,
-      ),
-      body: ListView.builder(
-        shrinkWrap: true,
-        itemCount: chats.length,
-        itemBuilder: (context, index) {
-          return oneChat(
-            name: chats[index]["name"],
-            lastMessage: chats[index]["lastMessage"],
-            time: chats[index]["time"],
-            imgURL: chats[index]["imgURL"],
-          );
-        },
-      ),
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: chats.length,
+      itemBuilder: (context, index) {
+        return oneChat(
+          name: chats[index]["name"],
+          lastMessage: chats[index]["lastMessage"],
+          time: chats[index]["time"],
+          imgURL: chats[index]["imgURL"],
+        );
+      },
     );
   }
 }
