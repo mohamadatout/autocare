f5d3028cb8579273687a6d4712cc400a15053970
MhmdAtout <mohamad.atout0@gmail.com>
2023-04-30 18:41:54 +0300

    frontend: removing scaffold and its app bar from profile screen

--- autocare_app/lib/screens/profile.page.dart
+++ autocare_app/lib/screens/profile.page.dart
@@ -1,3 +1,4 @@
+import 'package:autocare_app/routes/routes.dart';
 import 'package:autocare_app/widgets/button.dart';
 import 'package:autocare_app/widgets/store.bottomNavbar.dart';
 import 'package:flutter/material.dart';
@@ -28,73 +29,64 @@ class _ProfilePageState extends State<ProfilePage> {
   // }
 
   void signUserout() {
+    Navigator.of(context).popAndPushNamed(RouteManager.login);
     print("User signed out");
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: Text(
-          "Profile",
-          style: TextStyle(color: Colors.black),
-        ),
-      ),
-      body: SafeArea(
-        child: Container(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(50, 50, 50, 0),
-              child: Column(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: Image.asset(
-                      "assets/bmw.jpg",
-                      fit: BoxFit.cover,
-                      width: 120,
-                      height: 120,
-                    ),
+    return SafeArea(
+      child: Container(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(50, 50, 50, 0),
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.asset(
+                    "assets/bmw.jpg",
+                    fit: BoxFit.cover,
+                    width: 120,
+                    height: 120,
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Mohamad Atout",
-                    style: TextStyle(fontFamily: "Sora", fontSize: 20),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "mohamad.atout0@gmail.com",
-                    style: TextStyle(fontFamily: "Sora", fontSize: 16),
-                  ),
-                  SizedBox(height: 30),
-                  Button(
-                    onTap: () {},
-                    text: "Change Profile Picture",
-                    paddingVertical: 10,
-                    paddingHorizantal: 0,
-                  ),
-                  SizedBox(height: 30),
-                  Button(
-                    onTap: () {},
-                    text: "Dark Mode",
-                    paddingVertical: 10,
-                    paddingHorizantal: 0,
-                  ),
-                  SizedBox(height: 30),
-                  Button(
-                    onTap: signUserout,
-                    text: "Sign out",
-                    paddingVertical: 10,
-                    paddingHorizantal: 0,
-                  ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Mohamad Atout",
+                  style: TextStyle(fontFamily: "Sora", fontSize: 20),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "mohamad.atout0@gmail.com",
+                  style: TextStyle(fontFamily: "Sora", fontSize: 16),
+                ),
+                SizedBox(height: 30),
+                Button(
+                  onTap: () {},
+                  text: "Change Profile Picture",
+                  paddingVertical: 10,
+                  paddingHorizantal: 0,
+                ),
+                SizedBox(height: 30),
+                Button(
+                  onTap: () {},
+                  text: "Dark Mode",
+                  paddingVertical: 10,
+                  paddingHorizantal: 0,
+                ),
+                SizedBox(height: 30),
+                Button(
+                  onTap: signUserout,
+                  text: "Sign out",
+                  paddingVertical: 10,
+                  paddingHorizantal: 0,
+                ),
+              ],
             ),
           ),
         ),
