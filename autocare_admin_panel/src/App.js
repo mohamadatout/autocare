a7c3f1df15b718dbb738e573eee801dd51f02b0a
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-03 01:48:23 +0300

    frontend: defining all routes

--- autocare_admin_panel/src/App.js
+++ autocare_admin_panel/src/App.js
@@ -1,8 +1,36 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Navbar from "./scenes/global/Navbar";
+import Sidebar from "./scenes/global/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./scenes/dashboard";
+import Customer from "./scenes/users/customer";
+import AllUsers from "./scenes/users/allUsers";
+import Stores from "./scenes/users/stores";
+import Form from "./scenes/addStoreForm";
 
 function App() {
-	return <div className="App"></div>;
+	const [theme, colorMode] = useMode();
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className="app">
+					<Sidebar />
+					<main className="content">
+						<Navbar />
+						<Routes>
+							<Route path="/" element={<Dashboard />}></Route>
+							<Route path="/users" element={<AllUsers />}></Route>
+							<Route path="/customers" element={<Customer />}></Route>
+							<Route path="/stores" element={<Stores />}></Route>
+							<Route path="/addStore" element={<Form />}></Route>
+						</Routes>
+					</main>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
 }
 
 export default App;
