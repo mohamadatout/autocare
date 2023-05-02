115055d72c22fdaf9ec00a66c69224a5ce7aa366
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-03 02:12:17 +0300

    frontend: editing stat box structuring and styling

--- autocare_admin_panel/src/components/StatBoc.jsx
+++ autocare_admin_panel/src/components/StatBoc.jsx
@@ -7,33 +7,31 @@ const StatBox = ({ title, subtitle, icon }) => {
 
 	return (
 		<Box
-			width="100%"
-			m="0 30px"
+			width="80%"
+			height="80%"
 			display={"flex"}
 			flexDirection={"column"}
-			justifyContent={"center"}
+			justifyContent={"space-evenly"}
 			alignItems={"center"}>
-			<Box>
-				<Box>
-					<Box
-						display={"flex"}
-						flexDirection={"column"}
-						justifyContent={"center"}
-						alignItems={"center"}>
-						{icon}
-					</Box>
-					<Typography
-						display={"flex"}
-						flexDirection={"column"}
-						justifyContent={"center"}
-						alignItems={"center"}
-						variant="h4"
-						fontWeight="bold"
-						sx={{ color: colors.grey[100] }}>
-						{title}
-					</Typography>
-				</Box>
+			<Box
+				padding={"5px"}
+				display={"flex"}
+				flexDirection={"column"}
+				justifyContent={"center"}
+				alignItems={"center"}>
+				{icon}
 			</Box>
+			<Typography
+				display={"flex"}
+				flexDirection={"column"}
+				justifyContent={"center"}
+				alignItems={"center"}
+				variant="h4"
+				fontWeight="bold"
+				sx={{ color: colors.grey[100] }}>
+				{title}
+			</Typography>
+
 			<Box display="flex" justifyContent="space-between" mt="2px">
 				<Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
 					{subtitle}
