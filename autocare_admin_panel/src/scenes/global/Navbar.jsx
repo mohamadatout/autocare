680376f77bb1c74860fd4aab87d8e5b69968a0f8
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-03 01:40:43 +0300

    frontend: changing navbar's style

--- autocare_admin_panel/src/scenes/global/Navbar.jsx
+++ autocare_admin_panel/src/scenes/global/Navbar.jsx
@@ -1,9 +1,6 @@
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
-import { InputBase } from "@mui/material";
-import { dark } from "@mui/material/styles/createPalette";
-import SearchIcon from "@mui/icons-material/Search";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
 
@@ -12,17 +9,7 @@ const Navbar = () => {
 	const colors = tokens(theme.palette.mode);
 	const colorMode = useContext(ColorModeContext);
 	return (
-		<Box display={"flex"} justifyContent={"space-between"} p={2}>
-			<Box
-				display={"flex"}
-				borderRadius={"3px"}
-				backgroundColor={colors.primary[400]}>
-				<InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search"></InputBase>
-				<IconButton type="button" sx={{ p: 1 }}>
-					<SearchIcon />
-				</IconButton>
-			</Box>
-
+		<Box display={"flex"} justifyContent={"end"} p={2}>
 			<Box display={"flex"}>
 				<IconButton onClick={colorMode.toggleColorMode}>
 					{theme.palette.mode == "dark" ? <DarkModeIcon /> : <LightModeIcon />}
