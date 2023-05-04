c0c0c85aa7f307204c1b495602b5aecfc6b81632
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-04 21:28:34 +0300

    frontend: fixing dependecies and flutter related error

--- autocare_app/ios/Runner/AppDelegate.swift
+++ autocare_app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyD65SOqk_0MoQ2EXy75mBN8It983eogM-M")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
