d39b4be9fe32da7903ab38741668080bb50bdc4d
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-07 00:07:40 +0300

    frontend: adding a property for Product class

--- autocare_app/lib/models/products.model.dart
+++ autocare_app/lib/models/products.model.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Product {
-  String name;
-  String made;
-  String model;
-  String year;
-  double price;
-  String image;
-  String category;
+  final String id;
+  final String name;
+  final String made;
+  final String model;
+  final String year;
+  final String price;
+  final String image;
+  final String category;
 
   Product({
+    required this.id,
     required this.name,
     required this.made,
     required this.model,
@@ -18,24 +20,4 @@ class Product {
     required this.image,
     required this.category,
   });
-
-  void getProductFromMap(Map json) {
-    Product product = Product(
-      name: json["name"],
-      made: json["made"],
-      model: json["model"],
-      year: json["year"],
-      price: json["price"],
-      image: json["image"],
-      category: json["category"],
-    );
-
-    name = json["name"];
-    made = json["made"];
-    model = json["model"];
-    year = json["year"];
-    price = json["price"];
-    image = json["image"];
-    category = json["category"];
-  }
 }
