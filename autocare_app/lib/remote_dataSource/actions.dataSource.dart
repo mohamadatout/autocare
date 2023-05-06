3a902b9f45655ba8397ba0aa0f9270b25b4a4e75
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-06 11:04:17 +0300

    frontned: adding a datasource for user's actions

---
+++ autocare_app/lib/remote_dataSource/actions.dataSource.dart
@@ -0,0 +1,12 @@
+import 'package:autocare_app/config/local_storage.config.dart';
+import 'package:autocare_app/enums/localTypes.dart';
+
+abstract class ActionsDataSource {
+  // static Future getProducts(storeId) async {
+  //   try {
+  //
+  //   } catch (err) {
+  //     print(err);
+  //   }
+  // }
+}
