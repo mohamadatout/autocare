fffd9a5b47e841b4526134e4a9bfa78b9eaff5ee
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-06 11:01:43 +0300

    frontend: displaying user's info on profile screen

--- autocare_app/lib/screens/Profile.screen.dart
+++ autocare_app/lib/screens/Profile.screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:autocare_app/models/user.model.dart';
 import 'package:autocare_app/routes/routes.dart';
 import 'package:autocare_app/widgets/button.dart';
 import 'package:autocare_app/widgets/store.bottomNavbar.dart';
@@ -5,6 +8,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -14,19 +19,19 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  // File? image;
+  File? image;
 
-  // Future pickImage() async {
-  //   try {
-  //     final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-  //     if (image == null) return;
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
 
-  //     final imageTemporary = File(image.path);
-  //     setState(() => this.image = imageTemporary);
-  //   } on PlatformException catch (e) {
-  //     print("Failed to pick image : $e");
-  //   }
-  // }
+      final imageTemporary = File(image.path);
+      setState(() => this.image = imageTemporary);
+    } on PlatformException catch (e) {
+      print("Failed to pick image : $e");
+    }
+  }
 
   void signUserout() {
     Navigator.of(context).popAndPushNamed(RouteManager.login);
@@ -35,58 +40,60 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(50, 50, 50, 0),
-            child: Column(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(100),
-                  child: Image.asset(
-                    "assets/bmw.jpg",
-                    fit: BoxFit.cover,
-                    width: 120,
-                    height: 120,
+    return Consumer<LoggedUser>(
+      builder: (context, value, child) => SafeArea(
+        child: Container(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(50, 50, 50, 0),
+              child: Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Image.asset(
+                      "assets/bmw.jpg",
+                      fit: BoxFit.cover,
+                      width: 120,
+                      height: 120,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "Mohamad Atout",
-                  style: TextStyle(fontFamily: "Sora", fontSize: 20),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "mohamad.atout0@gmail.com",
-                  style: TextStyle(fontFamily: "Sora", fontSize: 16),
-                ),
-                SizedBox(height: 30),
-                Button(
-                  onTap: () {},
-                  text: "Change Profile Picture",
-                  paddingVertical: 10,
-                  paddingHorizantal: 0,
-                ),
-                SizedBox(height: 30),
-                Button(
-                  onTap: () {},
-                  text: "Dark Mode",
-                  paddingVertical: 10,
-                  paddingHorizantal: 0,
-                ),
-                SizedBox(height: 30),
-                Button(
-                  onTap: signUserout,
-                  text: "Sign out",
-                  paddingVertical: 10,
-                  paddingHorizantal: 0,
-                ),
-              ],
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    value.name,
+                    style: TextStyle(fontFamily: "Sora", fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    value.email,
+                    style: TextStyle(fontFamily: "Sora", fontSize: 16),
+                  ),
+                  SizedBox(height: 30),
+                  Button(
+                    onTap: pickImage,
+                    text: "Change Profile Picture",
+                    paddingVertical: 10,
+                    paddingHorizantal: 0,
+                  ),
+                  SizedBox(height: 30),
+                  Button(
+                    onTap: () {},
+                    text: "Dark Mode",
+                    paddingVertical: 10,
+                    paddingHorizantal: 0,
+                  ),
+                  SizedBox(height: 30),
+                  Button(
+                    onTap: signUserout,
+                    text: "Sign out",
+                    paddingVertical: 10,
+                    paddingHorizantal: 0,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
