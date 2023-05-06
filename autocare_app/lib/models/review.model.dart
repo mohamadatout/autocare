2b7b0cc87e7865eec1209f63a099e1fc498c9536
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-07 00:32:57 +0300

    frontend: adding property to the Review class

--- autocare_app/lib/models/review.model.dart
+++ autocare_app/lib/models/review.model.dart
@@ -1,8 +1,10 @@
 class Review {
+  final String id;
   final double customer;
   final String content;
 
   Review({
+    required this.id,
     required this.customer,
     required this.content,
   });
