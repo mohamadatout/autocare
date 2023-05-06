33bf9705cfeb68e5c0563798da7d425dc91c0493
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-06 10:46:35 +0300

    frontend: defining sendRequest function

--- autocare_app/lib/config/remote.config.dart
+++ autocare_app/lib/config/remote.config.dart
@@ -1,3 +1,6 @@
+import 'package:autocare_app/config/local_storage.config.dart';
+import 'package:autocare_app/enums/localTypes.dart';
+import 'package:autocare_app/enums/requestMethods.dart';
 import 'package:dio/dio.dart';
 
 final options = BaseOptions(
@@ -6,3 +9,36 @@ final options = BaseOptions(
 );
 
 final dioClient = Dio(options);
+
+Future sendRequest(
+    {required String route,
+    RequestMethods method = RequestMethods.GET,
+    Map? load}) async {
+  final String? token =
+      await getLocal(type: LocalTypes.String, key: "access_key");
+
+  if (token != null) {
+    final BaseOptions authorizeOptions =
+        dioClient.options.copyWith(headers: {"Authorization": "Bearer $token"});
+
+    dioClient.options = authorizeOptions;
+  }
+
+  if (method == RequestMethods.GET) {
+    final response = await dioClient.get(route, data: load);
+    return response;
+  } else if (method == RequestMethods.POST) {
+    final response = await dioClient.post(route, data: load);
+    return response;
+  } else if (method == RequestMethods.DELETE) {
+    final response = await dioClient.delete(route, data: load);
+    return response;
+  } else if (method == RequestMethods.PUT) {
+    final response = await dioClient.put(route, data: load);
+    return response;
+  }
+
+  return Future(() {
+    return "NULL";
+  });
+}
