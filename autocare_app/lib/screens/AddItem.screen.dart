57c84942e146906433407bc3b4e9ae9e0cfd3bbe
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-07 19:35:21 +0300

    frontend: adding add new item functionality

--- autocare_app/lib/screens/AddItem.screen.dart
+++ autocare_app/lib/screens/AddItem.screen.dart
@@ -1,11 +1,14 @@
+import 'package:autocare_app/models/user.model.dart';
 import 'package:autocare_app/widgets/button.dart';
 import 'package:autocare_app/widgets/inputField.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:provider/provider.dart';
 
 class AddItem extends StatefulWidget {
-  const AddItem({super.key});
+  final String storeId;
+  const AddItem({super.key, required this.storeId});
 
   @override
   State<AddItem> createState() => _AddItemState();
@@ -13,11 +16,23 @@ class AddItem extends StatefulWidget {
 
 class _AddItemState extends State<AddItem> {
   var nameCotroller = TextEditingController();
-  var categoryController = TextEditingController();
+  String categoryController = "Washing";
   var madeController = TextEditingController();
   var priceController = TextEditingController();
   var modelController = TextEditingController();
   var yearController = TextEditingController();
+
+  Future addItem() async {
+    await Provider.of<LoggedUser>(context, listen: false).addProduct(
+        widget.storeId,
+        nameCotroller.text,
+        madeController.text,
+        modelController.text,
+        yearController.text,
+        priceController.text,
+        "assets/bmw.jpg",
+        categoryController);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,10 +51,9 @@ class _AddItemState extends State<AddItem> {
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
             child: Column(
               children: [
-                inputField(controller: nameCotroller, hintText: "Name"),
                 inputField(
-                  controller: categoryController,
-                  hintText: "Category",
+                  controller: nameCotroller,
+                  hintText: "Name",
                 ),
                 inputField(
                   controller: madeController,
@@ -54,9 +68,41 @@ class _AddItemState extends State<AddItem> {
                   hintText: "Model",
                 ),
                 inputField(controller: yearController, hintText: "Year"),
+                DropdownButton<String>(
+                  value: categoryController,
+                  items: [
+                    const DropdownMenuItem(
+                      child: Text("Interior Parts"),
+                      value: "Interior",
+                    ),
+                    const DropdownMenuItem(
+                      child: Text("Exterior Parts"),
+                      value: "Exterior",
+                    ),
+                    const DropdownMenuItem(
+                      child: Text("Other Parts"),
+                      value: "Parts",
+                    ),
+                    const DropdownMenuItem(
+                      child: Text("Motor Parts"),
+                      value: "Motor",
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Washing"),
+                      value: "Washing",
+                    ),
+                  ],
+                  onChanged: (String? value) {
+                    setState(() {
+                      categoryController = value!;
+                    });
+                  },
+                ),
                 SizedBox(height: 30),
                 Button(
-                    onTap: () {},
+                    onTap: () {
+                      addItem();
+                    },
                     text: "Add Item",
                     paddingVertical: 18,
                     paddingHorizantal: 0)
