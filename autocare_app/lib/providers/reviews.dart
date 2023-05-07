155a6af99a47d759aeb3e1d72a631cd45d1d869d
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-07 23:10:57 +0300

    frontend: adding get stores reviews to the reviews provider

--- autocare_app/lib/providers/reviews.dart
+++ autocare_app/lib/providers/reviews.dart
@@ -1,4 +1,5 @@
 import 'package:autocare_app/models/review.model.dart';
+import 'package:autocare_app/remote_dataSource/load.dataSource.dart';
 import 'package:flutter/material.dart';
 
 class ReviewssProvider with ChangeNotifier {
@@ -12,5 +13,25 @@ class ReviewssProvider with ChangeNotifier {
       customer: json["customer"],
       review: json["review"],
     );
+  }
+
+  Future getStoresReviews(storeId) async {
+    try {
+      final res = await LoadDataSource.getStoreReviews(storeId);
+
+      List<Review> _reviews = [];
+
+      res.forEach((jsonStore) {
+        final Review product = fromJSON(jsonStore);
+
+        _reviews.add(product);
+      });
+
+      reviews = _reviews;
+
+      notifyListeners();
+    } catch (err) {
+      print(err);
+    }
   }
 }
