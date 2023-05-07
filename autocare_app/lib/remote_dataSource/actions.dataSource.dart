ee1dc071cf8b92da734bc0daf1ac3914537fd046
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-07 12:24:13 +0300

    frontend: creating add product method to fetch API

--- autocare_app/lib/remote_dataSource/actions.dataSource.dart
+++ autocare_app/lib/remote_dataSource/actions.dataSource.dart
@@ -1,12 +1,27 @@
 import 'package:autocare_app/config/local_storage.config.dart';
+import 'package:autocare_app/config/remote.config.dart';
 import 'package:autocare_app/enums/localTypes.dart';
+import 'package:autocare_app/enums/requestMethods.dart';
 
 abstract class ActionsDataSource {
-  // static Future getProducts(storeId) async {
-  //   try {
-  //
-  //   } catch (err) {
-  //     print(err);
-  //   }
-  // }
+  static Future addProduct(
+      storeId, name, made, model, year, price, image, category) async {
+    final body = {
+      "id": storeId,
+      "name": name,
+      "made": made,
+      "model": model,
+      "year": year,
+      "price": price,
+      "image": image,
+      "category": category
+    };
+    try {
+      final response = await sendRequest(
+          route: "/actions/addItem", load: body, method: RequestMethods.POST);
+      return response.data;
+    } catch (err) {
+      print(err);
+    }
+  }
 }
