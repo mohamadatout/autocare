fd92a5bb2bc962805ff85874ed5011c24878d002
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-07 23:17:58 +0300

    frontend: creating a theme toggler for the thme provider

---
+++ autocare_app/lib/providers/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider with ChangeNotifier {
+  bool isDark;
+  ThemeProvider({required this.isDark});
+
+  Future<void> toggleTheme() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    final check = prefs.getBool("isDark");
+
+    isDark = check ?? false;
+
+    notifyListeners();
+
+    prefs.setBool("isDark", !(check ?? false));
+  }
+}
