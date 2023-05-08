045716f24d0c9339be4bbb2ca1ad02867019a4cd
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-08 17:38:55 +0300

    backend: adding a route to the API

--- autocare-server/routes/admin.routes.js
+++ autocare-server/routes/admin.routes.js
@@ -4,9 +4,11 @@ const router = Router();
 const {
 	newStore,
 	changeSubscription,
+	getAllUsers,
 } = require("../controllers/admin.controller");
 
 router.post("/newStore", newStore);
 router.post("/changeSubscription", changeSubscription);
+router.get("/getAllUsers", getAllUsers);
 
 module.exports = router;
