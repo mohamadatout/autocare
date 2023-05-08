94c4a62cedb3d097f1fb0e054e70dc198bbd4717
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-08 17:55:51 +0300

    frontend: adding a list of favourite products to the page

--- autocare_app/lib/screens/Favourites.screen.dart
+++ autocare_app/lib/screens/Favourites.screen.dart
@@ -1,6 +1,11 @@
+import 'package:autocare_app/models/store.model.dart';
+import 'package:autocare_app/models/user.model.dart';
+import 'package:autocare_app/providers/stores.dart';
+import 'package:autocare_app/widgets/displayStoreProductCard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:provider/provider.dart';
 
 class FavouriteScreen extends StatefulWidget {
   const FavouriteScreen({super.key});
@@ -12,6 +17,33 @@ class FavouriteScreen extends StatefulWidget {
 class _FavouriteScreenState extends State<FavouriteScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Consumer<StoresProvider>(
+      builder: (context, storesProvider, child) =>
+          Consumer<LoggedUser>(builder: (context, value, child) {
+        return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Expanded(
+              child: ListView.builder(
+                physics: ScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: value.favourites.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      print("Hello");
+                    },
+                    child: StoreProductsDisplayCard(
+                      storeId: storesProvider.stores
+                          .firstWhere((store) => store.products.any((product) =>
+                              product.id == value.favourites[index].id))
+                          .id,
+                      productInfo: value.favourites[index],
+                    ),
+                  );
+                },
+              ),
+            ));
+      }),
+    );
   }
 }
