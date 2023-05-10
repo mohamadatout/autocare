8769894f1e329ee0caab2ac1b7b548402eb0026c
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-11 01:54:49 +0300

    frontend: adding navigation

--- autocare_app/lib/screens/AddItem.screen.dart
+++ autocare_app/lib/screens/AddItem.screen.dart
@@ -32,6 +32,7 @@ class _AddItemState extends State<AddItem> {
         priceController.text,
         "assets/bmw.jpg",
         categoryController);
+    Navigator.of(context).pop();
   }
 
   @override
@@ -39,74 +40,77 @@ class _AddItemState extends State<AddItem> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
         elevation: 0,
         title: Text(
           "Add Item",
           style: TextStyle(color: Colors.black),
         ),
       ),
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
-            child: Column(
-              children: [
-                inputField(
-                  controller: nameCotroller,
-                  hintText: "Name",
-                ),
-                inputField(
-                  controller: madeController,
-                  hintText: "Made",
-                ),
-                inputField(
-                  controller: priceController,
-                  hintText: "Price",
-                ),
-                inputField(
-                  controller: modelController,
-                  hintText: "Model",
-                ),
-                inputField(controller: yearController, hintText: "Year"),
-                DropdownButton<String>(
-                  value: categoryController,
-                  items: [
-                    const DropdownMenuItem(
-                      child: Text("Interior Parts"),
-                      value: "Interior",
-                    ),
-                    const DropdownMenuItem(
-                      child: Text("Exterior Parts"),
-                      value: "Exterior",
-                    ),
-                    const DropdownMenuItem(
-                      child: Text("Other Parts"),
-                      value: "Parts",
-                    ),
-                    const DropdownMenuItem(
-                      child: Text("Motor Parts"),
-                      value: "Motor",
-                    ),
-                    DropdownMenuItem(
-                      child: Text("Washing"),
-                      value: "Washing",
-                    ),
-                  ],
-                  onChanged: (String? value) {
-                    setState(() {
-                      categoryController = value!;
-                    });
-                  },
-                ),
-                SizedBox(height: 30),
-                Button(
-                    onTap: () {
-                      addItem();
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+              child: Column(
+                children: [
+                  inputField(
+                    controller: nameCotroller,
+                    hintText: "Name",
+                  ),
+                  inputField(
+                    controller: madeController,
+                    hintText: "Made",
+                  ),
+                  inputField(
+                    controller: priceController,
+                    hintText: "Price",
+                  ),
+                  inputField(
+                    controller: modelController,
+                    hintText: "Model",
+                  ),
+                  inputField(controller: yearController, hintText: "Year"),
+                  DropdownButton<String>(
+                    value: categoryController,
+                    items: [
+                      const DropdownMenuItem(
+                        child: Text("Interior Parts"),
+                        value: "Interior",
+                      ),
+                      const DropdownMenuItem(
+                        child: Text("Exterior Parts"),
+                        value: "Exterior",
+                      ),
+                      const DropdownMenuItem(
+                        child: Text("Other Parts"),
+                        value: "Parts",
+                      ),
+                      const DropdownMenuItem(
+                        child: Text("Motor Parts"),
+                        value: "Motor",
+                      ),
+                      DropdownMenuItem(
+                        child: Text("Washing"),
+                        value: "Washing",
+                      ),
+                    ],
+                    onChanged: (String? value) {
+                      setState(() {
+                        categoryController = value!;
+                      });
                     },
-                    text: "Add Item",
-                    paddingVertical: 18,
-                    paddingHorizantal: 0)
-              ],
+                  ),
+                  SizedBox(height: 30),
+                  Button(
+                      onTap: () {
+                        addItem();
+                      },
+                      text: "Add Item",
+                      paddingVertical: 18,
+                      paddingHorizantal: 0)
+                ],
+              ),
             ),
           ),
         ),
