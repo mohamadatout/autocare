184017f27919a30c74963af6faa20ed604cd6b15
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-11 02:06:40 +0300

    backend: adding routes for the APIs

--- autocare-server/routes/auth.routes.js
+++ autocare-server/routes/auth.routes.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
 const router = Router();
 
-const { register, login } = require("../controllers/auth.controller");
+const {
+	register,
+	login,
+	googleLogin,
+	googleRegister,
+} = require("../controllers/auth.controller");
 
 router.post("/register", register);
+router.post("/googleRegister", googleRegister);
 router.get("/login", login);
+router.get("/googleLogin", googleLogin);
 
 module.exports = router;
