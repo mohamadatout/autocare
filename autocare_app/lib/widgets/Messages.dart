9c47f8b0219720ac850e40343ee4292fa8c65f94
MhmdAtout <mohamad.atout0@gmail.com>
2023-05-11 01:56:36 +0300

    frontend: live chat

--- autocare_app/lib/widgets/Messages.dart
+++ autocare_app/lib/widgets/Messages.dart
@@ -1,60 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class Messages extends StatelessWidget {
-  List<Map> chats = [
-    {"messageContent": "Hii", "messageType": "sender"},
-    {"messageContent": "How are you", "messageType": "sender"},
-    {"messageContent": "Helooo", "messageType": "reciever"},
-    {"messageContent": "I'm fine", "messageType": "reciever"},
-    {
-      "messageContent":
-          "I just want tot ask if I can pass by at 5 to check on my car",
-      "messageType": "sender"
-    },
-    {"messageContent": "Yeah sure, your welcome :)", "messageType": "reciever"},
-  ];
-  // const OneMessage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 0, 0, 100),
-        child: ListView.builder(
-          physics: NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          itemCount: chats.length,
-          itemBuilder: (context, index) {
-            return Container(
+    final currentUser = FirebaseAuth.instance.currentUser;
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("chat")
+            .orderBy("createdAt", descending: true)
+            .snapshots(),
+        builder: (ctx, chatSnapchots) {
+          if (chatSnapchots.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (!chatSnapchots.hasData ||
+              chatSnapchots.data!.docChanges.isEmpty) {
+            return const Center(
+              child: Text("No Messages"),
+            );
+          }
+
+          final loadedMessages = chatSnapchots.data!.docs;
+
+          return ListView.builder(
+            padding: EdgeInsets.only(bottom: 100, left: 13, right: 13),
+            reverse: true,
+            itemCount: loadedMessages.length,
+            itemBuilder: (ctx, index) => Container(
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
               child: Align(
-                alignment: chats[index]["messageType"] == "reciever"
-                    ? Alignment.topLeft
-                    : Alignment.topRight,
+                alignment:
+                    loadedMessages[index].data()["userId"] != currentUser!.uid
+                        ? Alignment.topLeft
+                        : Alignment.topRight,
                 child: Container(
                   constraints: BoxConstraints(
                       maxWidth: MediaQuery.of(context).size.width - 130),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
-                    color: chats[index]["messageType"] == "reciever"
+                    color: loadedMessages[index].data()["userId"] !=
+                            currentUser.uid
                         ? Color.fromARGB(255, 246, 246, 246)
                         : Color.fromARGB(255, 187, 50, 51),
                   ),
                   padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                   child: Text(
-                    chats[index]["messageContent"],
-                    style: chats[index]["messageType"] == "sender"
+                    loadedMessages[index].data()["text"],
+                    style: loadedMessages[index].data()["userId"] ==
+                            currentUser.uid
                         ? TextStyle(fontSize: 18, color: Colors.white)
                         : TextStyle(color: Colors.black, fontSize: 18),
                   ),
                 ),
               ),
-            );
-          },
-        ),
-      ),
-    );
+            ),
+          );
+        });
   }
 }
